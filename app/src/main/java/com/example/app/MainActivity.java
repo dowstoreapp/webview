687475321647df0e53dd930d2d1fe0fd35357fbc
dowstoreapp <55833112+dowstoreapp@users.noreply.github.com>
2019-09-26 10:20:20 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends Activity {
         webSettings.setJavaScriptEnabled(true);
 
         // REMOTE RESOURCE
-        // mWebView.loadUrl("https://example.com");
+        // mWebView.loadUrl("https://dowstoreapp.blogspot.com/");
         // mWebView.setWebViewClient(new MyWebViewClient());
 
         // LOCAL RESOURCE
